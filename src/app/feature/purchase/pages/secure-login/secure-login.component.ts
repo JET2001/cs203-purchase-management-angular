26c414813147e636c4ac5eb4b77554a037727507
Jonathan-Teo <teo.junrui.jonathanedward2@gmail.com>
2023-10-01 01:50:21 +0800

    API calls are working

--- src/app/feature/purchase/pages/secure-login/secure-login.component.ts
+++ src/app/feature/purchase/pages/secure-login/secure-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from 'src/app/core/services/authentication/authentication.service';
 
 @Component({
   selector: 'app-secure-login',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class SecureLoginComponent {
 
+
+  isLoginSuccessful: boolean = false;
+  dataValue: string | boolean | undefined = undefined;
+
+  constructor(
+    private authService: AuthenticationService
+  ){}
+
+  handleLoginClick(){
+    let email: string = "jrteo.2022@smu.edu.sg";
+    let mobile: string = "06598231539";
+    let password: string = "test123";
+    this.authService.login(email, mobile, password).subscribe(
+      (data: string | boolean) => {
+        if (typeof data == typeof ""){ // received a JWT token
+          this.authService.saveAuthToken(data.toString()); // save JWT Token to browser local storage.
+          this.isLoginSuccessful = true;
+        }
+        this.dataValue = data;
+      }
+    )
+  }
 }
