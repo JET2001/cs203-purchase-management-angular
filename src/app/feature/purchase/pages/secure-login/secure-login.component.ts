1077c175090c5373abd51cd5ebd65ce54b3cd147
Ben <ashencheer@gmail.com>
2023-10-04 00:16:15 +0800

    implemented basic login features

--- src/app/feature/purchase/pages/secure-login/secure-login.component.ts
+++ src/app/feature/purchase/pages/secure-login/secure-login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthenticationService } from 'src/app/core/services/authentication/authentication.service';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { User } from 'src/app/models/user';
+
 
 @Component({
   selector: 'app-secure-login',
@@ -8,26 +11,64 @@ import { AuthenticationService } from 'src/app/core/services/authentication/auth
 })
 export class SecureLoginComponent {
 
+  loginFG: FormGroup;
+  emailFC: FormControl = new FormControl('', []);
+  mobileFC: FormControl = new FormControl('', []);
+  passwordFC: FormControl = new FormControl('', []);
+  checkboxFC: FormControl = new FormControl(false);
+
+  showInvalidLoginMessage: boolean = false;
 
   isLoginSuccessful: boolean = false;
   dataValue: string | boolean | undefined = undefined;
 
   constructor(
+    private fb: FormBuilder,
     private authService: AuthenticationService
-  ){}
+  ){
+    this.loginFG = this.fb.group({
+      email: this.emailFC,
+      mobile: this.mobileFC,
+      password: this.passwordFC,
+      checkbox: this.checkboxFC,
+    });
+  }
 
   handleLoginClick(){
-    let email: string = "jrteo.2022@smu.edu.sg";
-    let mobile: string = "06598231539";
-    let password: string = "test123";
-    this.authService.login(email, mobile, password).subscribe(
+    if (!this._fieldsAllValid()) return;
+    // let email: string = "jrteo.2022@smu.edu.sg";
+    // let mobile: string = "06598231539";
+    // let password: string = "test123";
+    let mobile = this.processMobile();
+    this.authService.login(this.emailFC.value, mobile, this.passwordFC.value).subscribe(
       (data: string | boolean) => {
         if (typeof data == typeof ""){ // received a JWT token
           this.authService.saveAuthToken(data.toString()); // save JWT Token to browser local storage.
           this.isLoginSuccessful = true;
-        }
+        } 
         this.dataValue = data;
       }
     )
+  }
+
+  private _fieldsAllValid(): boolean {
+    for (let value of [
+      this.emailFC.value,
+      this.mobileFC.value,
+      this.passwordFC.value,
+    ]) {
+      if (value == null || value == undefined || value == '') {
+        this.showInvalidLoginMessage = true;
+        return false;
+      }
+    }
+    this.showInvalidLoginMessage = false;
+    return true;
+  }
+
+  private processMobile(): string {
+    let mobile: string = this.mobileFC.value;
+    mobile = mobile.replace('+', '0');
+    return mobile;
   }
 }
