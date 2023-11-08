24d3b612a56f69bc61c1a493680f7c6461162d35
Clarissa Toh <clarissatoh03@gmail.com>
2023-11-08 15:39:22 +0800

    set up automatic deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ng build --configuration=production --aot 
-aws s3 cp ./dist s3://cs203-g4-team4-purchase-management-angular-test.com --recursive
+aws s3 cp ./dist s3://cs203-g4-team4-ticket-purchase-angular.com --recursive
